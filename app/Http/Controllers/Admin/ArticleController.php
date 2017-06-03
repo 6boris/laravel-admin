e3dccc3a241bd660d58e8af7184320f52c53ce72
小小一锥星 <280183608@qq.com>
2017-06-03 22:28:50 +0800

    blade 初始化

---
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ArticleController extends Controller
+{
+    public function article(){
+    	return view('admin.article.index');
+    }
+
+    public function addarticle(){
+
+    }
+
+    public function cate(){
+
+    }
+
+    public function addcate(){
+
+    }
+}
